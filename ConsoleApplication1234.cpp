2dda5b2bdc6fe1d97611d85afff8ab483655fdc1
tibanger <in.beleonok@gmail.com>
2023-02-28 00:09:13 +0200

    add some text in file

--- ConsoleApplication1234.cpp
+++ ConsoleApplication1234.cpp
@@ -13,6 +13,7 @@ int main()
 	if (a < 2 && b <= 3) cout << "Не справедлива (false)";
 	cout << " нерівність A > 2 та B ≥ 3" << endl;
 	cout << "I'm use git for this file!" << endl;
+	cout << "add some text" << endl;
 
 	return 0;
 }
