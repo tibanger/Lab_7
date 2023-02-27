55b0df7a7a7f986832984bcb8869a942cf714fe5
tibanger <in.beleonok@gmail.com>
2023-02-27 23:51:43 +0200

    Upload file (1)

---
+++ ConsoleApplication1234.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cout << "\nВведіть A: ";
+	cin >> a;
+	cout << "\nВведіть B: ";
+	cin >> b;
+	if (a > 2 && b >= 3) cout << "/nСправедлива (true)";
+	if (a < 2 && b <= 3) cout << "Не справедлива (false)";
+	cout << " нерівність A > 2 та B ≥ 3" << endl;
+	cout << "I'm use git for this file!" << endl;
+
+	return 0;
+}
