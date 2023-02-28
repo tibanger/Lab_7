511382baff9fb22c21e66270adc0b3dcbc7618f7
tibanger <in.beleonok@gmail.com>
2023-02-28 22:30:55 +0200

    Creat new branch, add new file

---
+++ For brTest.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cout << "Enter a:" << endl;
+	cin >> a;
+	cout << "Enter b:" << endl;
+	cin >> b;
+	cout << "a+b= " << a + b;
+
+	return 0;
+}
